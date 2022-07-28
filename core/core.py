eba55923ebfe198b24cb09677575ab75673804d2
Saeed <saeed.adelpour99@gmail.com>
2022-07-28 16:55:09 +0430

    add method for consume items

--- core/core.py
+++ core/core.py
@@ -18,3 +18,27 @@ class ProducerConsumer:
     def produce_all(self):
         for item in self.items:
             self.queue.put_nowait(item)
+
+    async def consume(self, result, method, args, kwargs):
+        while True:
+            # because we sure that queue will be filled completely, we can check queue.empty()
+            if len(result) == len(self.items):
+                self.cancel_tasks("consume")
+
+            item = await self.queue.get()
+            try:
+                item_result = await method(item, *args, **kwargs)
+                result.append(item_result)
+                self.queue.task_done()
+            except Exception:
+                logger.exception("error")
+                await self.queue.put(item)
+                self.queue.task_done()
+                return TaskResult.fail
+
+    def cancel_tasks(self, task_name):
+        tasks = asyncio.all_tasks()
+        for task in tasks:
+            coro = task.get_coro()
+            if coro.__name__ == task_name:
+                task.cancel()
