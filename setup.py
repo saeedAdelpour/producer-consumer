8034a49fb86617677794d9a2b86c4f6c972fc6a8
Saeed <saeed.adelpour99@gmail.com>
2022-08-18 11:37:51 +0430

    fix(setup): fix installing package bugs

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/saeedAdelpour/producer-consumer",
-    packages=setuptools.find_packages(),
+    packages=["producer_consumer"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
