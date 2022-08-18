265d1d94dc71d8e8cd0d605037e825a2e0fa87f1
Saeed <saeed.adelpour99@gmail.com>
2022-08-18 09:05:33 +0430

    add(setups): add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+
+setuptools.setup(
+    name="producer_consumer",
+    version="0.1",
+    # scripts=["dokr"],
+    author="saeed adelpour",
+    author_email="saeed@adelpour99@gmail.com",
+    description="An async producer consumer class",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/saeedAdelpour/producer-consumer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
